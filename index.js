552e9e0a0c39642e6a0c1c6823851bcb59b8cdae
shaikafrin <shaikafrin9848@gmail.com>
2023-09-29 17:50:29 +0530

    error solved

--- index.js
+++ index.js
@@ -12,29 +12,18 @@ document.querySelector('#search').onclick=() =>{
 
 
 var swiper = new Swiper(".product-row", {
-    spaceBetween: 30,
-    loop:true,
-    centeredSlides:true,
-    autoplay:{
-        delay:9500,
-        disableOnInteraction:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
+  slidesPerView: 3,
+   spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   });
+
 var swiper = new Swiper(".blogs-row", {
     spaceBetween: 30,
     loop:true,
@@ -85,6 +74,6 @@ var swiper = new Swiper(".blogs-row", {
       },
       1024: {
         slidesPerView: 3,
-      },
-    },
-  });
+      },
+    },
+  });
